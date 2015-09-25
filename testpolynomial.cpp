b3d3279b87c786da6c83baa55549bbe9a1f58849
Jonathan Ventura <jventura@uccs.edu>
2015-09-25 17:18:31 -0600

    Added dynamic test

--- testpolynomial.cpp
+++ testpolynomial.cpp
@@ -70,28 +70,29 @@ int main( int argc, char **argv )
     c.realRootsSturm(lb,ub,roots);
     std::cout << "roots using sturm sequences:\n";
     for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
+    
+    std::cout << "\n";
 
-//    std::cout << "*** Dynamically-sized polynomial roots test ***\n";
-//    
-//    Eigen::Matrix<double,5,1> ccoeffs;
-//    ccoeffs <<    -0.8049,    -0.4430,    0.0938,    0.9150,    0.9298;
-//    Polynomial<4> c(ccoeffs);
-//    
-//    std::cout << "c: [" << c.coefficients().transpose() << "]\n";
-//    
-//    double lb,ub;
-//    c.rootBounds(lb,ub);
-//    std::cout << "bounds on roots: " << lb << "  " << ub << "\n";
-//    
-//    std::vector<double> roots;
-//    c.realRoots(roots);
-//    std::cout << "roots using companion matrix:\n";
-//    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
-//    
-//    roots.clear();
-//    c.realRootsSturm(lb,ub,roots);
-//    std::cout << "roots using sturm sequences:\n";
-//    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
+    std::cout << "*** Dynamically-sized polynomial roots test ***\n";
+    
+    Eigen::VectorXd cdcoeffs(5);
+    cdcoeffs <<    -0.8049,    -0.4430,    0.0938,    0.9150,    0.9298;
+    Polynomial<Eigen::Dynamic> cd(ccoeffs);
+    
+    std::cout << "c: [" << cd.coefficients().transpose() << "]\n";
+    
+    cd.rootBounds(lb,ub);
+    std::cout << "bounds on roots: " << lb << "  " << ub << "\n";
+    
+    roots.clear();
+    cd.realRoots(roots);
+    std::cout << "roots using companion matrix:\n";
+    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
+    
+    roots.clear();
+    cd.realRootsSturm(lb,ub,roots);
+    std::cout << "roots using sturm sequences:\n";
+    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
 
     return 0;
 }
