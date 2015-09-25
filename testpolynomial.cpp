36eeb61a09243c9179158e4038cdf2385a6bce74
Jonathan Ventura <jventura@uccs.edu>
2015-09-25 16:41:24 -0600

    Updating test program

--- testpolynomial.cpp
+++ testpolynomial.cpp
@@ -14,14 +14,22 @@ int main( int argc, char **argv )
     bcoeffs << 1,2,3,4,5;
     Polynomial<4> b( bcoeffs );
 
-    std::cout << (a+b).coefficients().transpose() << "\n";
-    std::cout << (a-b).coefficients().transpose() << "\n";
+    std::cout << "*** Polynomial algebra test ***\n";
     
-    std::cout << (a*b).coefficients().transpose() << "\n";
+    std::cout << "a: [" << a.coefficients().transpose() << "]\n";
+    std::cout << "b: [" << b.coefficients().transpose() << "]\n";
+    
+    std::cout << "a+b: [" << (a+b).coefficients().transpose() << "]\n";
+    std::cout << "a-b: [" << (a-b).coefficients().transpose() << "]\n";
+    
+    std::cout << "a*b: [" << (a*b).coefficients().transpose() << "]\n";
 
-    std::cout << a.eval(5.f) << "\n";
+    std::cout << "a(5): " << a.eval(5.f) << "\n";
     
-    
+    std::cout << "\n";
+
+    std::cout << "*** Dynamically-sized polynomial algebra test ***\n";
+
     Eigen::VectorXd adcoeffs(6);
     adcoeffs << 1,2,3,4,5,6;
     Polynomial<Eigen::Dynamic> ad( adcoeffs );
@@ -29,71 +37,61 @@ int main( int argc, char **argv )
     bdcoeffs << 1,2,3,4,5;
     Polynomial<Eigen::Dynamic> bd( bdcoeffs );
     
-    std::cout << (ad+bd).coefficients().transpose() << "\n";
-    std::cout << (ad-bd).coefficients().transpose() << "\n";
+    std::cout << "a: [" << ad.coefficients().transpose() << "]\n";
+    std::cout << "b: [" << bd.coefficients().transpose() << "]\n";
+
+    std::cout << "a+b: [" << (ad+bd).coefficients().transpose() << "]\n";
+    std::cout << "a-b: [" << (ad-bd).coefficients().transpose() << "]\n";
     
-    std::cout << (ad*bd).coefficients().transpose() << "\n";
+    std::cout << "a*b: [" << (ad*bd).coefficients().transpose() << "]\n";
     
-    std::cout << ad.eval(5.f) << "\n";
+    std::cout << "a(5): " << ad.eval(5.f) << "\n";
+    
+    std::cout << "\n";
+    
+    std::cout << "*** Polynomial roots test ***\n";
     
     Eigen::Matrix<double,5,1> ccoeffs;
     ccoeffs <<    -0.8049,    -0.4430,    0.0938,    0.9150,    0.9298;
     Polynomial<4> c(ccoeffs);
     
-    std::vector<double> roots;
-    c.realRoots(roots);
-    std::cout << "companion matrix:\n";
-    for ( int i = 0; i < roots.size(); i++ ) std::cout << roots[i] << "\n";
-
+    std::cout << "c: [" << c.coefficients().transpose() << "]\n";
+    
     double lb,ub;
     c.rootBounds(lb,ub);
-    std::cout << "root bounds: " << lb << "  " << ub << "\n";
+    std::cout << "bounds on roots: " << lb << "  " << ub << "\n";
+    
+    std::vector<double> roots;
+    c.realRoots(roots);
+    std::cout << "roots using companion matrix:\n";
+    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
 
     roots.clear();
     c.realRootsSturm(lb,ub,roots);
-    std::cout << "sturm:\n";
-    for ( int i = 0; i < roots.size(); i++ ) std::cout << roots[i] << "\n";
+    std::cout << "roots using sturm sequences:\n";
+    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
 
-    exit(0);
-    
-    Eigen::Matrix<double,21,1> coef;
-    
-    FILE *f = fopen("/Users/jventura/code/solverlib/data/polydata.dat","r");
-    int deg;
-    fread(&deg,sizeof(int),1,f);
-    fread(coef.data(),sizeof(double),21,f);
-    fclose(f);
-    Polynomial<20> poly( coef );
-    
-    Eigen::VectorXd coefd( coef );
-    Polynomial<Eigen::Dynamic> polyd( coef );
-
-    poly.rootBounds(lb,ub);
-    std::cout << "root bounds: " << lb << "  " << ub << "\n";
-    
-    roots.clear();
-    poly.realRoots(roots);
-    std::cout << "companion matrix:\n";
-    for ( int i = 0; i < roots.size(); i++ ) std::cout << roots[i] << "\n";
-
-    roots.clear();
-    poly.realRootsSturm(lb,ub,roots);
-    std::cout << "sturm:\n";
-    for ( int i = 0; i < roots.size(); i++ ) std::cout << roots[i] << "\n";
-
-    double lbd,ubd;
-    polyd.rootBounds(lbd,ubd);
-    std::cout << "root bounds (dynamic): " << lbd << "  " << ubd << "\n";
-    
-    roots.clear();
-    polyd.realRoots(roots);
-    std::cout << "companion matrix (dynamic):\n";
-    for ( int i = 0; i < roots.size(); i++ ) std::cout << roots[i] << "\n";
-    
-    roots.clear();
-    polyd.realRootsSturm(lbd,ubd,roots);
-    std::cout << "sturm (dynamic):\n";
-    for ( int i = 0; i < roots.size(); i++ ) std::cout << roots[i] << "\n";
+//    std::cout << "*** Dynamically-sized polynomial roots test ***\n";
+//    
+//    Eigen::Matrix<double,5,1> ccoeffs;
+//    ccoeffs <<    -0.8049,    -0.4430,    0.0938,    0.9150,    0.9298;
+//    Polynomial<4> c(ccoeffs);
+//    
+//    std::cout << "c: [" << c.coefficients().transpose() << "]\n";
+//    
+//    double lb,ub;
+//    c.rootBounds(lb,ub);
+//    std::cout << "bounds on roots: " << lb << "  " << ub << "\n";
+//    
+//    std::vector<double> roots;
+//    c.realRoots(roots);
+//    std::cout << "roots using companion matrix:\n";
+//    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
+//    
+//    roots.clear();
+//    c.realRootsSturm(lb,ub,roots);
+//    std::cout << "roots using sturm sequences:\n";
+//    for ( int i = 0; i < roots.size(); i++ ) std::cout << "c(" << roots[i] << "): " << c.eval(roots[i]) << "\n";
 
     return 0;
 }
